a1f26e99b7adf567ea317316d326faaa947d9c68
Benji161220 <benje1612@gmail.com>
2025-10-21 11:49:29 +0200

    Aplicacion Terminada

--- app/src/main/java/com/example/elsol/CompararPlanetasActivity.kt
+++ app/src/main/java/com/example/elsol/CompararPlanetasActivity.kt
@@ -1,51 +1,64 @@
 package com.example.elsol
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.AdapterView.OnItemClickListener
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
-
 class CompararPlanetasActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comparar_planetas)
 
-        val listaPlanetas: MutableList<Planeta?> = ArrayList<Planeta?>()
-        listaPlanetas.add(Planeta("Mercurio", 0.382, 0.387, 5400))
-        listaPlanetas.add(Planeta("Venus", 0.949, 0.723, 5250))
-        listaPlanetas.add(Planeta("Tierra", 1.0, 1.0, 5520))
-        listaPlanetas.add(Planeta("Marte", 0.53, 1.542, 3960))
-        listaPlanetas.add(Planeta("Júpiter", 11.2, 5.203, 1350))
-        listaPlanetas.add(Planeta("Saturno", 9.41, 9.539, 700))
-        listaPlanetas.add(Planeta("Urano", 3.38, 19.81, 1200))
-        listaPlanetas.add(Planeta("Neptuno", 3.81, 30.07, 1500))
-        listaPlanetas.add(Planeta("Plutón", 0.0, 39.44, 5))
-        val adapter = ArrayAdapter<Planeta?>(
-            this,
-            android.R.layout.simple_dropdown_item_1line, listaPlanetas
-        )
-        val auto = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView1)
+        val auto1 = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView1)
+        val auto2 = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView2)
 
-        auto.setAdapter(adapter)
-        auto.threshold = 1
-        auto.setOnItemClickListener(object : OnItemClickListener {
-            override fun onItemClick(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                val planetaSeleccionado =
-                    auto.getAdapter().getItem(position) as Planeta
-                diametro1.setText(planetaSeleccionado.diametro.toString())
-                distancia1.setText(planetaSeleccionado.distanciaAlSol.toString())
-                densidad1.setText(planetaSeleccionado.densidad.toString())
-            }
-        })
+        val diametro1 = findViewById<TextView>(R.id.diametro1)
+        val distancia1 = findViewById<TextView>(R.id.distancia1)
+        val densidad1  = findViewById<TextView>(R.id.densidad1)
+
+        val diametro2 = findViewById<TextView>(R.id.diametro2)
+        val distancia2 = findViewById<TextView>(R.id.distancia2)
+        val densidad2  = findViewById<TextView>(R.id.densidad2)
+
+        // 2) Datos (no nulos; más simple)
+        val listaPlanetas = mutableListOf(
+            Planeta("Mercurio", 0.382, 0.387, 5400),
+            Planeta("Venus",    0.949, 0.723, 5250),
+            Planeta("Tierra",   1.0,   1.0,   5520),
+            Planeta("Marte",    0.53,  1.542, 3960),
+            Planeta("Júpiter",  11.2,  5.203, 1350),
+            Planeta("Saturno",  9.41,  9.539, 700),
+            Planeta("Urano",    3.38,  19.81, 1200),
+            Planeta("Neptuno",  3.81,  30.07, 1500),
+            Planeta("Plutón",   0.0,   39.44, 5)
+        )
+
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, listaPlanetas)
+        auto1.setAdapter(adapter)
+        auto2.setAdapter(adapter)
+        auto1.threshold = 1
+        auto2.threshold = 1
+
+        fun renderA(p: Planeta) {
+            diametro1.text = p.diametro.toString()
+            distancia1.text = p.distanciaAlSol.toString()
+            densidad1.text = p.densidad.toString()
+        }
+        fun renderB(p: Planeta) {
+            diametro2.text = p.diametro.toString()
+            distancia2.text = p.distanciaAlSol.toString()
+            densidad2.text = p.densidad.toString()
+        }
+
+        auto1.setOnItemClickListener { parent, _, position, _ ->
+            val planeta = parent.adapter.getItem(position) as Planeta
+            renderA(planeta)
+        }
+        auto2.setOnItemClickListener { parent, _, position, _ ->
+            val planeta = parent.adapter.getItem(position) as Planeta
+            renderB(planeta)
+        }
     }
 }
-
--- app/src/main/java/com/example/elsol/Planeta.kt
+++ app/src/main/java/com/example/elsol/Planeta.kt
@@ -1,9 +1,9 @@
 package com.example.elsol
-
-
-class Planeta (
-    val nombre1: String,
-    val diametro1: Double,
-    val distancia1: Double,
-    val densidad1: Int
-)
+data class Planeta(
+    val nombre: String,
+    val diametro: Double,
+    val distanciaAlSol: Double,
+    val densidad: Int
+) {
+    override fun toString(): String = nombre
+}
